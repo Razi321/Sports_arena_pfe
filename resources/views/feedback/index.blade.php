e9f952ad30cf81d9f06441f7c8395ad8d8f6c52c
unknown <razi melliti>
2020-06-04 14:55:14 +0100

    changes in feedback list

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -3,25 +3,16 @@
 
 @extends('layouts.dashboardAdmin')
 @section('content')
-<h1>feedbackq</h1>
-
-<div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-12" >
-        <a href="/users/create" class="btn btn-primary">Ajouter utiliasateur</a>
-      </div>
-    </div>
-  </div>
-
+<h1>les avis</h1>
 
   @if(count($feedbacks) > 0)
   @foreach($feedbacks as $feedback)
 
     <div class ='card p-3 mt-3 mb-3'>
             <h2 >   <a href="/gyms/{{$feedback->belongs_to}}">{{$feedback->body}}</a></h2>
-            <small>Written on {{$feedback->created_at}} </small>
-
-
+            <small> <span class="txt_bold"> Client</span> :   {{$feedback->usersfeed->name}} </small><br>
+            <small> <span  class="txt_bold"> salle de sport  </span>: {{$feedback->gym->name}} </small> <br>
+            <small> <span  class="txt_bold"> Créé le </span> : {{$feedback->created_at}} </small>
 
 
     </div>
